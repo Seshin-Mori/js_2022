43e85f1163009888a7965b785c540629a3df3590
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2022-12-21 00:00:54 +0900

    再開

--- Basic/main.js
+++ Basic/main.js
@@ -1,14 +1,43 @@
 //test
 //alert('hello');
 
+//かなり簡易的にやり直す。知らないことを確認する程度
+
 let count;
 //undefined
-console.log(count);
+//console.log(count);
 count = 30;
-console.log(count);
+//console.log(count);
 let newCount = 0;
 
 let additionResult = 2 + 5;
-console.log(additionResult);
+//console.log(additionResult);
+
+let string = 'Hello'
+//特殊文字を使う場合は\
+const userName = `test\\`;
+string += userName + '!';
+//console.log(string);
+//$`{}`で変数埋め込み
+//console.log(`${string}`);
+
+const userInput = '10.9';
+let calcResult;
+
+calcResult = Number(userInput) + 10;
+//小数点以下なし
+calcResult = parseInt(userInput) + 10;
+//float 20.9
+calcResult = parseFloat(userInput) + 10;
+console.log(calcResult);
+
+//配列
+let array = ['apple', 'banana'];
+array = [1,2,3];
+array = ['apple', 'banana', 'grape'];
+console.log( array[0]);
+console.log( array);
+
+//オブジェクト
 
 
