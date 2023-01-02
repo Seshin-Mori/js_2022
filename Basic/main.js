46201e298a79c7c8a8a3a9bd74cbf042cd020490
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2023-01-02 23:48:15 +0900

    セクション1の終了

--- Basic/main.js
+++ Basic/main.js
@@ -13,15 +13,15 @@ let newCount = 0;
 let additionResult = 2 + 5;
 //console.log(additionResult);
 
-let string = 'Hello'
+let string = "Hello";
 //特殊文字を使う場合は\
 const userName = `test\\`;
-string += userName + '!';
+string += userName + "!";
 //console.log(string);
 //$`{}`で変数埋め込み
 //console.log(`${string}`);
 
-const userInput = '10.9';
+const userInput = "10.9";
 let calcResult;
 
 calcResult = Number(userInput) + 10;
@@ -32,12 +32,51 @@ calcResult = parseFloat(userInput) + 10;
 console.log(calcResult);
 
 //配列
-let array = ['apple', 'banana'];
-array = [1,2,3];
-array = ['apple', 'banana', 'grape'];
-console.log( array[0]);
-console.log( array);
+let array = ["apple", "banana"];
+array = [1, 2, 3];
+array = ["apple", "banana", "grape"];
+console.log(array[0]);
+console.log(array);
 
 //オブジェクト
+const coffee = {
+  //key:value
+  //プロパティ名:値
+  name: "Chocolate",
+  size: 350,
+  isHot: true,
+  toppings: ["Cinnamon", "Caramel"],
+  nutrition: {
+    calories: 430,
+    sugars: 53,
+  },
+};
+coffee.isHot = false;
+console.log(coffee.size);
 
+//nullとundefined
+//nullは基本的に明示的に書くが、undefinedは暗黙的に様々な場面で登場する。
+let userInfo = null;
+//undefinedはundefined型
+//nullはオブジェクトである
 
+//パラメータ、仮引数
+function add(num1, num2) {
+  //console.log(1 + 1);
+  const value = num1 + num2;
+  console.log(value);
+  return value;
+  //return以下の処理は実行されない
+  //console.log(num1 + num2);
+}
+
+//add(4, 6);
+
+//関数を宣言をする前に呼び出すことは可能
+//関数宣言文はすべて内部的に巻き上げられる（内部的にファイルの一番上になる）。
+const returnedValue = add(2, 3);
+console.log(returnedValue);
+
+//これはできない。関数内の変数はスコープの外だから。
+//ブロックスコープ
+//console.log(value);
